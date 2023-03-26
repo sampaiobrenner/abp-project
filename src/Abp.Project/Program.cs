0953c18be0d0db7b252b0c08a2c4a67f406c47cf
Matheus Brenner <matheus.brenner@softplan.com.br>
2023-03-25 22:06:44 -0300

    Ajustando configuração do projeto

--- src/Abp.Project/Program.cs
+++ src/Abp.Project/Program.cs
@@ -1,15 +1,9 @@
-namespace Abp.Project;
+using Abp.Project;
 
-public static class Program
-{
-    public static async Task Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Host.AddAppSettingsSecretsJson().UseAutofac();
-        await builder.AddApplicationAsync<AppModule>();
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.AddAppSettingsSecretsJson().UseAutofac();
+await builder.AddApplicationAsync<AppModule>();
         
-        var app = builder.Build();
-        await app.InitializeApplicationAsync();
-        await app.RunAsync();
-    }
-}
+var app = builder.Build();
+await app.InitializeApplicationAsync();
+await app.RunAsync();
