8210a2b6dae67d1462c55c4c95802016ed973703
Matheus Brenner <matheus.brenner@softplan.com.br>
2023-03-26 17:00:48 -0300

    POC - ABP/Redis

--- src/Abp.Project/Controllers/ProductController.cs
+++ src/Abp.Project/Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using Abp.Project.Entities;
 using Abp.Project.Services.Products;
 using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
@@ -20,5 +21,12 @@ public class ProductController : AbpController
     {
         await _productService.AddAsync(cancellationToken);
         return Ok();
+    }
+    
+    [HttpGet]
+    public async Task<Product> GetAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var product = await _productService.GetAsync(id, cancellationToken);
+        return product;
     }
 }
--- src/Abp.Project/Entities/Product.cs
+++ src/Abp.Project/Entities/Product.cs
@@ -1,7 +1,9 @@
-﻿using Volo.Abp.Domain.Entities;
+﻿using Volo.Abp.Caching;
+using Volo.Abp.Domain.Entities;
 
 namespace Abp.Project.Entities;
 
+[CacheName(nameof(Product))]
 public class Product : Entity<Guid>
 {
     public string Name { get; set; }
